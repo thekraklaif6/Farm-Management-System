c5330168646857a513a51b32a0669bb098d42f53
eman_thaher <palestinep357@gmail.com>
2025-10-14 09:04:35 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/BransCont.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BransCont extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/NewViewController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewViewController  implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
